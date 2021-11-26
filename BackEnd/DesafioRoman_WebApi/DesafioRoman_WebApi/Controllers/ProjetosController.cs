141801d09e273abae59c3eea81210b5355b050b4
MurilloAssis <murillo98asiss@gmail.com>
2021-11-26 08:44:26 -0300

    Banco atualizado

---
+++ BackEnd/DesafioRoman_WebApi/DesafioRoman_WebApi/Controllers/ProjetosController.cs
@@ -0,0 +1,32 @@
+﻿using DesafioRoman_WebApi.Domains;
+using DesafioRoman_WebApi.Interfaces;
+using DesafioRoman_WebApi.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioRoman_WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjetosController : ControllerBase
+    {
+        private IProjetoRepository _projetoRepository { get; set; }
+
+        public ProjetosController()
+        {
+            _projetoRepository = new ProjetoRepository();
+        }
+
+        [HttpGet]
+        public IActionResult ListarProjetos()
+        {
+            List<Projeto> lista = _projetoRepository.ListarProjetos();
+            return Ok(lista);
+        }
+    }
+}
